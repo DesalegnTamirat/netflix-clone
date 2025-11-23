2eea9316490de4cb9fe080c226801e7886139616
Desalegn Tamirat <desalegntamirateshetu@gmail.com>
2025-11-24 00:58:56 +0300

    add basic styling to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,32 +4,39 @@ import {
   MdNotificationsNone,
   MdSearch,
 } from "react-icons/md";
+import "./header.css";
 
 function Header() {
   return (
-    <div>
-      <ul>
-        <li>Home</li>
-        <li>TV Shows</li>
-        <li>Movies</li>
-        <li>Latest</li>
-        <li>My List</li>
-        <li>Browse by Languages</li>
-      </ul>
-      <ul>
-        <li>
-          <MdSearch />
-        </li>
-        <li>
-          <MdNotificationsNone />
-        </li>
-        <li>
-          <MdAccountBox />
-        </li>
-        <li>
-          <MdArrowDropDown />
-        </li>
-      </ul>
+    <div className="header-outer-container place-center">
+      <div className="header-container place-between">
+        <div className="header-left">
+          <ul className="place-between">
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>My List</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        <div className="header-right">
+          <ul className="place-between">
+            <li>
+              <MdSearch />
+            </li>
+            <li>
+              <MdNotificationsNone />
+            </li>
+            <li>
+              <MdAccountBox />
+            </li>
+            <li>
+              <MdArrowDropDown />
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
