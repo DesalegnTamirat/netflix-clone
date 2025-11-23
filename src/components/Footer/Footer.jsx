f095988b4931eb06ee8b9d06d88d5f995a9ad937
Desalegn Tamirat <desalegntamirateshetu@gmail.com>
2025-11-24 01:34:42 +0300

    add footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,35 +1,42 @@
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
+import "./footer.css";
+
 function Footer() {
   return (
-    <div>
-      <div>
-        <FacebookOutlinedIcon />
-        <InstagramIcon />
-        <YouTubeIcon />
+    <div className="footer-outer-container justify-center">
+      <div className="footer-container">
+        <div className="footer-icons justify-between">
+          <FaFacebook />
+          <FaInstagram />
+          <FaYoutube />
+        </div>
+        <div className="footer-data justify-between">
+          <ul>
+            <li>Audio Description</li>
+            <li>Investor Relations</li>
+            <li>Legal Notice</li>
+          </ul>
+          <ul>
+            <li>Help Center</li>
+            <li>Jobs</li>
+            <li>Cookie Preferences</li>
+          </ul>
+          <ul>
+            <li>Gift Cards</li>
+            <li>Terms of Use</li>
+            <li>Corporate Information</li>
+          </ul>
+          <ul>
+            <li>Media Center</li>
+            <li>Privacy</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <p className="service-code place-center">Service Code</p>
+        <p className="copy-write">
+          &copy; {new Date().getFullYear()} Netflix, Inc.
+        </p>
       </div>
-      <div className="footer_data">
-        <ul>
-          <li>Audio Description</li>
-          <li>Investor Relations</li>
-          <li>Legal Notice</li>
-        </ul>
-        <ul>
-          <li>Help Center</li>
-          <li>Jobs</li>
-          <li>Cookie Preferences</li>
-        </ul>
-        <ul>
-          <li>Gift Cards</li>
-          <li>Terms of Use</li>
-          <li>Corporate Information</li>
-        </ul>
-        <ul>
-          <li>Media Center</li>
-          <li>Privacy</li>
-          <li>Contact Us</li>
-        </ul>
-      </div>
-      <p>Service Code</p>
-      <p>&copy; 1997-2024 Netflix, Inc.</p>
     </div>
   );
 }
