fbc43c0978e02a08662901d9cba0118e4bdd5066
Desalegn Tamirat <desalegntamirateshetu@gmail.com>
2025-11-24 01:17:28 +0300

    add footer jsx skeleton

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,37 @@
+function Footer() {
+  return (
+    <div>
+      <div>
+        <FacebookOutlinedIcon />
+        <InstagramIcon />
+        <YouTubeIcon />
+      </div>
+      <div className="footer_data">
+        <ul>
+          <li>Audio Description</li>
+          <li>Investor Relations</li>
+          <li>Legal Notice</li>
+        </ul>
+        <ul>
+          <li>Help Center</li>
+          <li>Jobs</li>
+          <li>Cookie Preferences</li>
+        </ul>
+        <ul>
+          <li>Gift Cards</li>
+          <li>Terms of Use</li>
+          <li>Corporate Information</li>
+        </ul>
+        <ul>
+          <li>Media Center</li>
+          <li>Privacy</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+      <p>Service Code</p>
+      <p>&copy; 1997-2024 Netflix, Inc.</p>
+    </div>
+  );
+}
+
+export default Footer;
