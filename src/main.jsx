27ee76ccdf6d7b28a860e734570e2578e3b39fd2
Desalegn Tamirat <desalegntamirateshetu@gmail.com>
2025-12-10 02:46:03 +0300

    change things

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/netflix-clone/">
+    <BrowserRouter basename="/movie-streaming-website/">
       <App />
     </BrowserRouter>
   </StrictMode>
